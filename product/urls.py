38402e64efce1f5c4d6a0bdbbedb9907bc07820d
eb-cmoyano <cmoyano@eventbrite.com>
2018-12-30 10:52:47 -0300

    add templates

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('view/<int:pk>', views.ProductView.as_view(), name='product_view'),
+    path('new', views.ProductCreate.as_view(), name='product_new'),
+    path('view/<int:pk>', views.ProductView.as_view(), name='product_view'),
+    path('edit/<int:pk>', views.ProductUpdate.as_view(), name='product_edit'),
+    path('delete/<int:pk>', views.ProductDelete.as_view(), name='product_delete'),
 ]
