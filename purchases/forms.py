7a31c2e40244e479bc470c334aa71b906673a051
eb-cmoyano <cmoyano@eventbrite.com>
2019-01-01 21:24:53 -0300

    add purchases

---
+++ purchases/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+
+from .models import (
+    Purchase,
+    Provider,
+)
+
+class PurchaseForm(forms.ModelForm):
+
+    class Meta:
+        model = Purchase
+        fields = [
+            'provider',
+            'additional_notes',
+            'gross_amount',
+            'tax',
+            'discount',
+            'shipping',
+            'paid_amount',
+            'status',
+            'payment_method',
+            'currency',
+            'invoice_url',
+        ]
+
+class ProviderForm(forms.ModelForm):
+
+    class Meta:
+        model = Provider
+        fields = [
+            'first_name',
+            'last_name',
+            'mobile_number',
+            'work_number',
+            'email',
+            'company',
+            'web_url',
+            'country',
+            'region',
+            'city',
+            'zip_code',
+            'street_address_1',
+            'street_address_2',
+            'address_note',
+            'additional_notes',
+        ]
--- purchases/urls.py
+++ purchases/urls.py
@@ -1,1 +1,19 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Purchase
+    path('', views.PurchaseList.as_view(), name='purchase_list'),
+    path('view/<int:pk>', views.PurchaseView.as_view(), name='purchase_view'),
+    path('new', views.PurchaseCreate.as_view(), name='purchase_new'),
+    path('view/<int:pk>', views.PurchaseView.as_view(), name='purchase_view'),
+    path('edit/<int:pk>', views.PurchaseUpdate.as_view(), name='purchase_edit'),
+    path('delete/<int:pk>', views.PurchaseDelete.as_view(), name='purchase_delete'),
+    # Provider
+    path('provider', views.ProviderList.as_view(), name='provider_list'),
+    path('provider/view/<int:pk>', views.ProviderView.as_view(), name='provider_view'),
+    path('provider/new', views.ProviderCreate.as_view(), name='provider_new'),
+    path('provider/view/<int:pk>', views.ProviderView.as_view(), name='provider_view'),
+    path('provider/edit/<int:pk>', views.ProviderUpdate.as_view(), name='provider_edit'),
+    path('provider/delete/<int:pk>', views.ProviderDelete.as_view(), name='provider_delete'),
+]
